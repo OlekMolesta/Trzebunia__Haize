191be9480205a2bddf5e3d5c8192e233be1afbd6
OlekMolesta <138704374+OlekMolesta@users.noreply.github.com>
2023-07-05 19:41:15 +0200

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,13 @@
+var languages = ["webpage/gacek.html","webpage/beniek.html","webpage/drobny.html","webpage/ernest.html","webpage/faraon.html","webpage/filmun.html","webpage/karakan.html","webpage/ksawery.html",
+"webpage/lechu.html","webpage/noname.html","webpage/ocet.html","webpage/prezes.html","webpage/tomasz.html","webpage/maciek.html","webpage/illia.html"];
+var output = languages[Math.floor(Math.random() * 13)];
+document.getElementById('button').onclick = function (){
+    button.style.display = 'none';
+    let field1 = document.getElementById('field');
+    let out = '';
+        out += `<a class="btn" role="alert" href="${output}">Lets go</a>`
+        field1.innerHTML = out;
+}
+
+
+
