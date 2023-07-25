14ccb064fd77036779911bd119b33470ec257f90
OlekMolesta <138704374+OlekMolesta@users.noreply.github.com>
2023-07-25 16:10:22 +0200

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,7 @@
 var languages = ["webpage/gacek.html","webpage/beniek.html","webpage/faraon.html","webpage/filmun.html","webpage/karakan.html","webpage/ksawery.html",
 "webpage/lechu.html","webpage/noname.html","webpage/ocet.html","webpage/prezes.html","webpage/tomasz.html","webpage/maciek.html","webpage/illia.html","webpage/franek.html",
-"webpage/tomek.html"];
-var output = languages[Math.floor(Math.random() * 15)];
+"webpage/tomek.html","webpage/pawel.html"];
+var output = languages[Math.floor(Math.random() * 16)];
 document.getElementById('button').onclick = function (){
     button.style.display = 'none';
     let field1 = document.getElementById('field');
